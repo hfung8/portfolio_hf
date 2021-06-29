f8e987a8c3a7a9b09912c41b2cdf8e71efa20332
Harrison Fung <harrison.hok.fung@gmail.com>
2021-06-29 16:04:00 -0400

    Updates

---
+++ precache-manifest.2d4b5cd8081ac602185b7c7d80e8cbc3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab8a850bd28cccbcd1e28b435740d7b",
+    "url": "/portfolio_hf/index.html"
+  },
+  {
+    "revision": "55373a63fd12075bae73",
+    "url": "/portfolio_hf/static/css/2.67731326.chunk.css"
+  },
+  {
+    "revision": "cb031d60645a9a801fa3",
+    "url": "/portfolio_hf/static/css/main.caf8f8cf.chunk.css"
+  },
+  {
+    "revision": "55373a63fd12075bae73",
+    "url": "/portfolio_hf/static/js/2.8d2a0384.chunk.js"
+  },
+  {
+    "revision": "cb031d60645a9a801fa3",
+    "url": "/portfolio_hf/static/js/main.abd153b7.chunk.js"
+  },
+  {
+    "revision": "edb96b6ffef1b7dbf632",
+    "url": "/portfolio_hf/static/js/runtime-main.7b092bf5.js"
+  },
+  {
+    "revision": "c17186cc5a1ab764c24077c99abb9d18",
+    "url": "/portfolio_hf/static/media/7seconds.c17186cc.png"
+  },
+  {
+    "revision": "c938d0a3afb11b1b7d9dd4e5fd4d5ac8",
+    "url": "/portfolio_hf/static/media/Card_game.c938d0a3.png"
+  },
+  {
+    "revision": "fdd703b356dd334fa2fbbe034202e9ec",
+    "url": "/portfolio_hf/static/media/OHU.fdd703b3.png"
+  },
+  {
+    "revision": "6a1d6cbd783eeb0bca98dd59afe38458",
+    "url": "/portfolio_hf/static/media/Open_weather.6a1d6cbd.png"
+  },
+  {
+    "revision": "98be41423d42a4c3cd6ef9bafa350b6c",
+    "url": "/portfolio_hf/static/media/YumCache.98be4142.png"
+  },
+  {
+    "revision": "74aa02f2091f426199c6a4caa70a2f1d",
+    "url": "/portfolio_hf/static/media/harrycircle.74aa02f2.png"
+  }
+]);
